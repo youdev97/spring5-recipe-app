b85d1234c7732fd5213fe12f6d4c60f4b53a5dc9
Younes Belcaid <younes.belcaid@businessdecision.be>
2021-01-25 17:35:51 +0100

    setting up ManyToMany relationship

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,7 +1,20 @@
 package guru.springframework.domain;
 
-import javax.persistence.*;
 import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 /**
  * Created by jt on 6/13/17.
@@ -26,12 +39,16 @@ public class Recipe {
 
 	@Lob
 	private Byte[] image;
-	
+
 	@Enumerated(value = EnumType.STRING)
 	private Difficulty difficulty;
 
 	@OneToOne(cascade = CascadeType.ALL)
 	private Notes notes;
+
+	@ManyToMany
+	@JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories;
 
 	public Long getId() {
 		return id;
