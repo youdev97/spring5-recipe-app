78843273af8418c5cca2a051c10de447cc2beb90
Younes Belcaid <younes.belcaid@businessdecision.be>
2021-01-26 11:10:42 +0100

    populate the db and used spring data JPA to retrieve categories and unit of measure by field description

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,16 +1,42 @@
 package guru.springframework.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 
 /**
  * Created by jt on 6/1/17.
  */
 @Controller
 public class IndexController {
+	
+	private CategoryRepository categoryRepository;
+	private UnitOfMeasureRepository unitOfMeasureRepository;
+	
+	
 
-    @RequestMapping({"", "/", "/index"})
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
+
+
+	@RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+		
+		Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+		Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+		
+		System.out.println("Cat Id is: " + categoryOptional.get().getId());
+		System.out.println("UOM Id is: " + unitOfMeasureOptional.get().getId());
+		
         return "index";
     }
 }
--- src/main/java/guru/springframework/repositories/CategoryRecipe.java
+++
@@ -1,9 +0,0 @@
-package guru.springframework.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import guru.springframework.domain.Category;
-
-public interface CategoryRecipe extends CrudRepository<Category, Long>{
-
-}
