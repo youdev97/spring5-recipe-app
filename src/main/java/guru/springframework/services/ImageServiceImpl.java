df9fd66623614a89c787e6e57c882fa7cfc182ac
Younes Belcaid <younes.belcaid@businessdecision.be>
2021-02-01 13:14:35 +0100

    persist image to db

--- src/main/java/guru/springframework/services/ImageServiceImpl.java
+++ src/main/java/guru/springframework/services/ImageServiceImpl.java
@@ -1,18 +1,51 @@
 package guru.springframework.services;
 
+import java.io.IOException;
+
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
 	
-	@Override
-	public void saveImageFile(Long recipeId, MultipartFile file) {
+	private final RecipeRepository recipeRepository;
 
-		log.debug("Received a file");
-	}
+    public ImageServiceImpl( RecipeRepository recipeService) {
+
+        this.recipeRepository = recipeService;
+    }
+
+    @Override
+    @Transactional
+    public void saveImageFile(Long recipeId, MultipartFile file) {
+
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()){
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            //todo handle better
+            log.error("Error occurred", e);
+
+            e.printStackTrace();
+        }
+    }
 	
 }
